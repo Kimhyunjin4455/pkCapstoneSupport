1897c97f341d86846c5bfb119cda6036e075dd4b
Kimhyunjin4455 <dp4455@naver.com>
2023-09-29 19:17:21 +0900

    0929createDtoFolder

--- src/main/java/org/zerock/freview/repository/FoodReviewRepository.java
+++ src/main/java/org/zerock/freview/repository/FoodReviewRepository.java
@@ -1,8 +1,13 @@
 package org.zerock.freview.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.freview.entity.FoodReview;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface FoodReviewRepository extends JpaRepository<FoodReview, Long> {
+
+
 }
--- src/test/java/org/zerock/freview/repository/FoodReviewRepositoryTests.java
+++ src/test/java/org/zerock/freview/repository/FoodReviewRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.freview.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.freview.entity.FoodReview;
+import org.zerock.freview.entity.QFoodReview;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -24,16 +31,33 @@ public class FoodReviewRepositoryTests {
 //        });
 //    }
 
-    @Test
-    public void updateTest(){
-        Optional<FoodReview> result = foodReviewRepository.findById(5L);
+//    @Test
+//    public void updateTest(){
+//        Optional<FoodReview> result = foodReviewRepository.findById(5L);
+//
+//        if (result.isPresent()){
+//            FoodReview foodReview = result.get();
+//            foodReview.changeRestaurantName("변경할 식당명 작성....");
+//            foodReview.changeContent("변경할 리뷰 내용 작성....");
+//
+//            foodReviewRepository.save(foodReview); //
+//        }
+//    }
 
-        if (result.isPresent()){
-            FoodReview foodReview = result.get();
-            foodReview.changeRestaurantName("변경할 식당명 작성....");
-            foodReview.changeContent("변경할 리뷰 내용 작성....");
-
-            foodReviewRepository.save(foodReview); //
-        }
-    }
+//    @Test
+//    public void testQuery1(){ // 문제1: findAll(builder,pageable)가 인식되지 않음
+//        // 지점명/내용과 같이 단 하나의 항목으로 검색하는 경우
+//        // 지점명 + 내용과 같이 2개의 항목으로 검색하는 경우
+//        // queryDsl 사용법: 1. BooleanBuilder 생성 2. 조건에 맞는 구문은 queryDsl에서 사용하는 Predicate타입의 함수 생성 3. BooleanBuilder에 작성된 Predicate 추가하고 실행
+//        Pageable pageable = PageRequest.of(0, 5, Sort.by("fno").descending());
+//        QFoodReview qFoodReview = QFoodReview.foodReview;
+//        String keyword = "1";
+//        BooleanBuilder builder = new BooleanBuilder();
+//        BooleanExpression expression = qFoodReview.restaurantName.contains(keyword);
+//        builder.and(expression);
+//        Page<FoodReview> result = foodReviewRepository.findAll(builder,pageable);
+//        result.stream().forEach(foodReview -> {
+//            System.out.println(foodReview);
+//        });
+//    }
 }
