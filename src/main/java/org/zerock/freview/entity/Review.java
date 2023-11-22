17ebaea4e383937122ed8e956c6b1b236a1859f1
Kimhyunjin4455 <dp4455@naver.com>
2023-11-22 18:16:45 +0900

    📰: review엔티티 추가

---
+++ src/main/java/org/zerock/freview/entity/Review.java
@@ -0,0 +1,22 @@
+package org.zerock.freview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "foodReivew")
+public class Review extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewNum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private FoodReview foodReview;
+
+    private String text;
+}
